ae84b87d8882fa9f0240f72677e91e78a10d4537
Ievgen Khoptiar <ievgen.khoptiar@gmail.com>
2019-10-06 16:18:38 +0300

    environments added with mock   values

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:4000',
+  firebaseConfig: {
+    apiKey: 'your-firebase-data',
+    authDomain: 'your-firebase-data',
+    databaseURL: 'your-firebase-data',
+    projectId: 'your-firebase-data',
+    storageBucket: 'your-firebase-data',
+    messagingSenderId: 'your-firebase-data',
+    appId: 'your-firebase-data'
+  }
+};
